4fb875b0b16f1d67f12e90fe3e9d797eab9fbb50
Adrian Enrique Castañeda Garagatti <acastaneda.1200@gmail.com>
2021-04-23 01:53:45 -0500

    test

--- src/components/PokemonItem.js
+++ src/components/PokemonItem.js
@@ -1,37 +1,50 @@
-import React, { useState } from 'react'
-import { fetchPokemon } from '../helpers/getPokemon'
+import React, { useEffect, useState } from 'react'
+//import { fetchPokemon } from '../helpers/getPokemon'
 
 
 export const PokemonItem = () => {
 
-    const [state, setState] = useState({
-        data: [],
+    const [pokemon, setPokemon] = useState({})
 
-    })
+    
+    useEffect(()=> {
+        fetchPokemons();
+    }, [])
   
+     const getPokemon = async (idPokemon) => {
 
 
-    fetchPokemon().then( poke => {
-        setState({
-            data: poke,
-        })
-    })
+        const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`);
+        const data = await url.json();
+        
      
-    console.log(state);
-    
-   
+        
+        
+        console.log(data);
+        setPokemon(data)
+        
+      }
 
-   
-    
+      const fetchPokemons = async () => {
+        for (let i = 1; i < 10; i++) {
+      
+          await getPokemon(i);
+        }
+      
+      }
+  
     return (
-        <div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title"></h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <>
+          
+                    <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{pokemon.name}</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
                 </div>
-            </div>
-        </div>
+             
+          
+        </>
     )
 }
